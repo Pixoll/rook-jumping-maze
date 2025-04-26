4e78b65ddf7566037ebb49c335dda075a418f051
Pixoll <pixoll.gamer@gmail.com>
2025-04-26 18:07:10 -0400

    Read input and turn into graphs

---
+++ src/main.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+from src.graph import Graph
+
+
+def main() -> None:
+    root_path = Path(__file__).parent.parent.resolve()
+    input_path = root_path / "input.txt"
+    lines: list[str] = []
+
+    with open(input_path) as input_file:
+        lines += input_file.read().splitlines()
+
+    line_index = 0
+
+    while line_index < len(lines) and lines[line_index].strip() != "0":
+        rows, cols, start_i, start_j, goal_i, goal_j = map(int, lines[line_index].split())
+        line_index += 1
+        matrix: list[list[int]] = []
+
+        for i in range(rows):
+            values = list(map(int, lines[line_index].split()))
+            if len(values) != cols:
+                raise Exception(f"Expected {cols} columns in line {line_index}")
+
+            matrix.append(values)
+            line_index += 1
+
+        graph = Graph(matrix, (start_i, start_j), (goal_i, goal_j))
+        print(graph)
+
+
+if __name__ == '__main__':
+    main()
